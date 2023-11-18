ce2c29c69ce6ae7aa9d3f82caf646acf98b990f5
KallQaQ <2298994920@qq.com>
2023-11-18 22:22:08 +0800

    函数练习1

---
+++ 函数学习/函数学习/储存反向字符串到字符数组函数.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include<math.h> 
+#include<string.h>
+
+void reverseString(char str[])
+{
+    int len = strlen(str);
+    for (int i = 0; i < len / 2; i++) 
+    {
+        char temp = str[i];
+        str[i] = str[len - i - 1];
+        str[len - i - 1] = temp;
+    }
+}
+
+int main()
+{
+    char str[100];
+    printf("Enter a string: ");
+    scanf("%[^\n]%*c", str);
+    reverseString(str);
+    printf("Reversed string: %s\n", str);
+    return 0;
+}
