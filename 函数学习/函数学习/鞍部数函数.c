2f5793088882a2609b11be79e3e0be03b31e5955
KallQaQ <2298994920@qq.com>
2023-11-20 21:52:32 +0800

    函数练习

--- 函数学习/函数学习/鞍部数函数.c
+++ 函数学习/函数学习/鞍部数函数.c
@@ -2,3 +2,18 @@
 #include <stdio.h>
 #include<math.h> 
 #include<string.h>
+
+int saddle_point(int a[100][100])
+{
+	int i, j, k,row,col;
+	row = sizeof(a) / sizeof(a[0]);
+	col = sizeof(a[0]) / sizeof(a[0][0]);
+	printf("%d,%d", row, col);
+}
+
+int main()
+{
+	int arr[3][3] = { {1,2,3},{1,2,3},{1,2,3} };
+	saddle_point(arr[3][3]);
+	
+}
