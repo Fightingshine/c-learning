4eaa8ca36889e04f482b1821c95ea89c001d8736
KallQaQ <2298994920@qq.com>
2023-12-01 18:52:38 +0800

    2

--- 指针学习/指针练习1/指针练习1/1.c
+++ 指针学习/指针练习1/指针练习1/1.c
@@ -1,10 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include<string.h>
+#include<math.h>
 
 
 int main()
 {
-	int arr[10] = {0};
-	char a[10] = { 1,1 };
-	printf("%d", &arr[0] - &a[0]);
+	int i,n,;
+	doubble arr[100] = {0},temp;
+	double* pa = arr;
+	printf("how many numbers you want to sort");
+	scanf("%d", &n);
+	printf("input numbers you want to sort");
+	for (i = 0; i < n; i++)
+	{
+		scanf("%f", pa + i);
+	}
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			printf
+		}
+	}
 }
